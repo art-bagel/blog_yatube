5e05965c8350be1b62f58c2e08cab4db1c70f979
art-bagel <mr.bagel@yandex.ru>
2022-04-21 04:57:41 +0300

    финальный проект

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -31,7 +31,8 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include('debug_toolbar.urls'))
     ] + urlpatterns
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
